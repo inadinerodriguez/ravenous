96bb0af73fd7955b406ac70a2ccdebabc7742435
Nadine Rodriguez <inadinerod@gmail.com>
2022-09-18 11:28:04 -0400

    Create SearchBar component

--- src/components/Business/BusinessList/BusinessList.js
+++ src/components/Business/BusinessList/BusinessList.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./BusinessList.css";
 import Business from "../Business";
 
-class BusinessList extends from React.Component {
+class BusinessList extends React.Component {
     render(){
         return (
             <div className="BusinessList">
