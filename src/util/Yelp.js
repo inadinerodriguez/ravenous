3913faba9df3fd86db0c1aa5854f66708b92db10
Nadine Rodriguez <inadinerod@gmail.com>
2022-09-21 15:58:50 -0400

    Install polfill to package.json and create Yelp Module

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,7 @@
 const apiKey = "yJlzBIAQpMnGZy1KuXKDNbF3bDp8-0LM06cCwFPPTujzsstwGnpMNK3cChPF6wSxp9s5AZhunBPnt5CfSGVz7CctKITmh491MZiNdJOnOH0X39UZDcPOmYPpsWorY3Yx";
+
+const Yelp = {};
+
+const search = function(term, location, sortBy) {
+
+};
